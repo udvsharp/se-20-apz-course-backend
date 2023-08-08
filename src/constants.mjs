057699947fed85b967533e791c83b213bdc21a73
Dmitry Vasyliev <dv4code@gmail.com>
2023-08-08 23:31:53 +0300

    Changed MongoDB config

--- src/constants.mjs
+++ src/constants.mjs
@@ -3,10 +3,11 @@ import crypto from 'crypto';
 
 // Auth
 export const secretKey = process.env.JWT_SECRET_KEY || crypto.randomBytes(32).toString('hex');
+export const mongoDbUri = process.env.MONGODB_URI;
 
 // DB
 export const dbName = 'prodef-db';
-export const dbUri = `mongodb://127.0.0.1:27017/${dbName}`;
+export const dbUri = `${mongoDbUri}/${dbName}`;
 export const dbOptions = { useNewUrlParser: true, useUnifiedTopology: true };
 
 // App
